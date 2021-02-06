05dbae6df2db6dd88f2a25b073aa7dbe4feae37f
Serik <serj_navii@mail.ru>
2021-02-06 09:33:25 +0600

    init: write function check for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = str.split('');
+  // максимум возможные варианты обхода циклом 2 раза меньше длины списка так как при удалении убирается по два элемента
+  let halfLength = Math.floor(brackets.length/2);
+  for(let j=0; j<halfLength; j++){
+    for(let i=0; i<bracketsConfig.length; i++){
+      let pairOfBrackets = bracketsConfig[i].join('');
+      // спиок преобразуем в строку и находим центральные парные скопки
+      let index = brackets.join('').indexOf(pairOfBrackets); 
+      if(index !== -1) {
+        brackets.splice(index, 2); // если есть пара скобок расположенные рядом то их вырезаем из списка
+      }
+    }
+  }
+  return brackets.length === 0;
 }
