411f4de2ec68aba59beb9842ddd5f0df27fed1dd
Serik <serj_navii@mail.ru>
2021-02-20 08:13:58 +0600

    refactor: optimize code to work faster

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = str.split('');
+  let brackets = str;
   // максимум возможные варианты обхода циклом 2 раза меньше длины списка так как при удалении убирается по два элемента
   let halfLength = Math.floor(brackets.length/2);
   for(let j=0; j<halfLength; j++){
     for(let i=0; i<bracketsConfig.length; i++){
       // преобразовываем пару скобок каждого элемента в строку для поиска в строке str
-      let pairOfBrackets = bracketsConfig[i].join('');
-      // спиок преобразуем в строку и находим центральные парные скопки
-      let index = brackets.join('').indexOf(pairOfBrackets); 
-      if(index !== -1) {
-        brackets.splice(index, 2); // если есть пара скобок расположенные рядом то их вырезаем из списка
-      }
+      // Меняем рядом стоящих скобок на пустую строку (удаляем парные скобки)
+      brackets = brackets.replace(bracketsConfig[i].join(''), '');
     }
   }
   return brackets.length === 0;
